63c9e5d31d74cd97241db76c3374f525f1a616f9
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-07-06 14:40:21 +0100

    Added function documentation

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -7,6 +7,8 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Complex function - returns a function"""
     def multiply(value: float) -> float:
+        """Multiplies two floats"""
         return value * multiplier
     return multiply
