d0b23fc971bf356e0d8f1b8b2fdd59c51749ebc9
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-07-06 13:47:20 +0100

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Returns the floor of a number given to the function."""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floor of the number given"""
+    return math.floor(n)
