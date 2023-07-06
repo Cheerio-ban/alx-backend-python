2f86ae7672ac18b983b41b57958a8aa48ed0a8a3
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-07-06 13:50:27 +0100

    To string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that converts a float to string.
+"""
+
+
+def to_str(n: float) -> str:
+    """Returns string representation of n passed"""
+    return str(n)
