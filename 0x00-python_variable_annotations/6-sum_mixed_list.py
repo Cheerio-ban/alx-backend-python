ed7bac106876217b3aba08daa96210e22289055d
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-07-06 14:11:24 +0100

    Typed annotated mixed list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Type annotated function that takes in a list of mixed type.
+"""
+
+from typing import Union
+
+
+def sum_mixed_list(mxd_lst: Union[float, int]) -> float:
+    """Returns sum of a mixed type of list as float"""
+    total: float = 0.00
+    for elem in mxd_lst:
+        total += elem
+    return total
