83de0e0322b0586b86dfdf41a87d96eab6ddcfba
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-07-06 19:32:08 +0100

    Annotations

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Annotations mehn!"""
+
+from typing import List, Tuple
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Returns a list"""
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
