f485a20caac63d28ed53dd7f58582c9987417f01
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-07-06 13:39:57 +0100

    Concatenates two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+This module contains a typed annotated function that concatenates two strings.
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings."""
+    return str1 + str2
