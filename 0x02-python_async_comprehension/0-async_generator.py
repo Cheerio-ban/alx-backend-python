ecd0d3aa6fc99f74e891a3bc2ec215b6ff01ebce
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-07-11 07:08:33 +0100

    async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" async generator """
+
+import random
+import asyncio
+
+
+async def async_generator():
+    """ Asynchronous Generator """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield(random.uniform(0, 10))
